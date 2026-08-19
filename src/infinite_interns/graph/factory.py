7c8ad64b1e7ebfd88630f69ae432048727df4831
Marco Paredes <marco.paredes2001@gmail.com>
2026-08-19 18:22:34 -0400

    style: apply Ruff import grouping

--- src/infinite_interns/graph/factory.py
+++ src/infinite_interns/graph/factory.py
@@ -1,8 +1,9 @@
 """Compiled parent graph exported to the LangGraph Agent Server."""
 
+from langgraph.graph import END, START, StateGraph
+
 from infinite_interns.graph.nodes import load_run, schedule, wait_or_finish
 from infinite_interns.graph.state import FactoryState
-from langgraph.graph import END, START, StateGraph
 
 
 _builder = StateGraph(FactoryState)
